df0bde7261ada11a01018d5efa40fa8f0bef5ad1
Steven Taing <steven.taing22@gmail.com>
2025-09-29 21:22:15 +0930

    Create Find_Most_Frequent_Vowel_and_Consonant.cpp

---
+++ Leetcode/Easy/Find_Most_Frequent_Vowel_and_Consonant.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int sum{ 0 };
+        int highestAltitude = sum;
+
+        for (int i : gain) {
+            sum += i;
+            if (sum > highestAltitude) highestAltitude = sum;
+        }
+
+        return highestAltitude;
+    }
+};
