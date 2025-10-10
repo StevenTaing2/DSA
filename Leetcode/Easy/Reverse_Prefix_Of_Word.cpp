d117711fdeee389b3510c6b16e17d4ea2796d4d4
Steven Taing <steven.taing22@gmail.com>
2025-10-10 19:56:41 +1030

    Create Reverse_Prefix_Of_Word.cpp

---
+++ Leetcode/Easy/Reverse_Prefix_Of_Word.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int i = word.find(ch);
+        if (i != -1) {
+            reverse(word.begin(), word.begin() + i + 1);
+        }
+        return word;
+    }
+};
