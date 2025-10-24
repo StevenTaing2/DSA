cf24fbe5d73e9c8e8b4612d6910d1dd5584cc50f
Steven Taing <steven.taing22@gmail.com>
2025-10-25 08:59:30 +1030

    Create Search_In_A_BST.cpp

---
+++ Leetcode/Easy/Search_In_A_BST.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode temp;
+        TreeNode* current = root;
+
+        if (current == nullptr) {
+            return nullptr;
+        }
+        if (current->val == val) {
+            return root;
+        }
+
+        while (current != nullptr) {
+            if (current->val == val) {
+                return current;
+            }
+
+            if (current->val > val) {
+                current = current->left;
+            }
+            else {
+                current = current->right;
+            }
+
+        }
+
+        return nullptr;
+
+    }
+};
